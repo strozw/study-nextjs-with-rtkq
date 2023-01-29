aafd6a137f296d2fe8fb22dbdd881d0ceaf3c32f
Satoru Ohzawa <606372+strozw@users.noreply.github.com>
2023-01-30 05:29:00 +0900

    feat: add storybook (#8)
    Co-authored-by: strozw <strozw@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
+import React from 'react'
 import './globals.css'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
