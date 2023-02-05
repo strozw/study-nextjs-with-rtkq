5b626e3464d7981338070e21ee21e54097bc452e
strozw <strozw@users.noreply.github.com>
2023-02-05 18:02:26 +0900

    test: 🚨 fix d3 module resolution in jest

--- jest.config.js
+++ jest.config.js
@@ -8,11 +8,18 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
   moduleDirectories: ['node_modules', '<rootDir>/'],
 
   moduleNameMapper: {
     '@/(.*)$': '<rootDir>/src/$1',
+    /**
+     * 最新の d3 の module が esm として解決されてしまうため、min.js (common.js) を見るようにする
+     * @see {@url https://github.com/facebook/jest/issues/12036#issuecomment-1039439333}
+     */
+    '^d3-(.*)$': '<rootDir>/node_modules/d3-$1/dist/d3-$1.min.js',
   },
+
   testEnvironment: 'jest-environment-jsdom',
 }
 
