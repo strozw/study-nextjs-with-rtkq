b96cbc8a13448a6873f76a4176d1b009a7f6e4ba
strozw <strozw@users.noreply.github.com>
2023-02-05 21:14:38 +0900

    feat: ✨ 都道府県が未選択状態のときの表示を追加

--- src/features/prefecturesPopulationChart/components/PrefecturesPopulationChartPage/PopulationXYChart/PopulationXYChart.css.ts
+++ src/features/prefecturesPopulationChart/components/PrefecturesPopulationChartPage/PopulationXYChart/PopulationXYChart.css.ts
@@ -1,7 +1,21 @@
 import { style } from '@vanilla-extract/css'
 
+import { vars } from '@/common/style/theme.css'
+
 export const wrapper = style({
   width: '100%',
   height: '500px !important',
   overflowX: 'scroll',
+  border: `0.2rem solid ${vars.color.lightGray1}`,
+})
+
+export const empty = style({
+  display: 'flex',
+  width: '100%',
+  height: '100%',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '40px',
+  background: vars.color.lightGray1,
+  fontSize: vars.fontSizes.l,
 })
